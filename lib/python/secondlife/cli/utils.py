bc3e4a175b62ad28769dfd8d4011de5dd4e97259
Maciej Grela <enki@fsck.pl>
2021-05-09 22:28:03 +0200

    secondlife: Simplify stdin, cmdline and filtered cell id handling

--- lib/python/secondlife/cli/utils.py
+++ lib/python/secondlife/cli/utils.py
@@ -4,21 +4,61 @@ import parsedatetime
 import time
 import sys
 from structlog import get_logger
+from pathlib import Path
+from secondlife.celldb import find_cell, load_metadata, new_cell
 
 log = get_logger()
 
 def cell_identifiers(config):
+
     for id in config.identifiers:
+            if id == '-':
+                for line in sys.stdin:
+                    line = line.strip()
+                    if len(line) == 0:
+                        continue
 
-        if id == '-':
-            for line in sys.stdin:
-                line = line.strip()
-                if len(line) == 0:
-                    continue
-
+                    yield line
+            else:
                 yield line
-        else:
-            yield id
+
+def include_cell(path, metadata, config):
+    # Check if cell is to be included based on configured criteria
+
+    if config.tags <= metadata.get('/tags', set()):
+        return True
+    else:
+        return False
+
+    return True
+
+def selected_cells(config):
+
+    if config.all_cells:
+        path = Path()
+        log.info('searching for cells', path=path)
+
+        # Now look in subdirectories
+        for path in path.glob('**/meta.json'):
+            try:
+                metadata = load_metadata(path)
+                if metadata.get('/id'):
+                    log.debug('cell found', id=metadata.get('/id'), path=path, meatadata=metadata)
+                    if include_cell(path, metadata, config=config):
+                        yield (path, metadata)
+            except Exception as e:
+                log.error('cannot load metadata', path=path, _exc_info=e)
+
+    for id in cell_identifiers(config=config):
+
+        # Find cell
+        path, metadata = find_cell(id)
+        if not path:
+            path, metadata = new_cell(id=line)
+
+        log.debug('cell found', id=metadata.get('/id'), path=path, meatadata=metadata)
+        if include_cell(path, metadata, config=config):
+            yield (path, metadata)
 
 def measurement_ts(config):
     # Parse the timestamp argument
@@ -31,3 +71,8 @@ def measurement_ts(config):
         sys.exit(1)
 
     return time.mktime(time_parsed)
+
+import argparse
+class AddSet(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        getattr(namespace, self.dest).add(values)
