7a12a68c4e3e2e3cd8d29b1d1707166200d60620
Maciej Grela <enki@fsck.pl>
2021-05-09 22:24:01 +0200

    secondlife: Add two basic reports

---
+++ lib/python/secondlife/plugins/capacity.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+from secondlife.plugins.api import v1
+from structlog import get_logger
+from dateutil.relativedelta import relativedelta
+import asciitable
+import json
+import time
+
+# We care only about hour level accuracy
+_attrs = ['years', 'months', 'days', 'hours']
+def _human_readable(delta):
+    try:
+        return ' '.join([ '%d %s' % (getattr(delta, attr), getattr(delta, attr) > 1 and attr or attr[:-1])
+                for attr in _attrs if getattr(delta, attr) ])
+    except:
+        return ''
+
+def _format_results(results):
+    r = []
+    for (name,value) in results.items():
+        r.append(f"{name}={value['v']}{value.get('u', value.get('unit'))}")
+    
+    return ' '.join(r)
+
+class CapacityReport(object):
+
+    def __init__(self, **kwargs):
+        self.config = kwargs['config']
+        self.log = get_logger(name=__class__.__name__)
+        self.rows = []
+
+    def process_cell(self, path, metadata):
+        log = self.log.bind(path=path, metadata=metadata)
+        log.debug('processing cell')
+
+        try:
+            log_path = path.with_name('log.json')
+            measurement_log = json.loads(log_path.read_text(encoding='utf8'))
+        except Exception as e:
+            log.error('cannot read log', _exc_info=e)
+            return
+    
+
+        # log.debug('measurement log', log=measurement_log)
+    
+        rows = []
+        for m in measurement_log:
+            rows.append([
+                _human_readable(relativedelta(seconds=m.get('ts')-time.time())) if 'ts' in m else '',
+                _format_results(m.get('results'))
+            ])
+
+        # print(f"Cell measurement log:")
+        # asciitable.write(rows, names=['Timestamp', 'Results'], Writer=asciitable.FixedWidth)
+
+        try:
+            capacity_measurement = next(filter(lambda m: 'capacity' in m['results'], measurement_log))
+            self.rows.append([ metadata.get('/id'), capacity_measurement['results']['capacity']['v'] ])
+        except StopIteration:
+            log.warn('no capacity measurement', metadata=metadata, path=path)
+            pass
+
+    def report(self):
+
+        if len(self.rows) > 0:
+            asciitable.write(self.rows, 
+                names=['Cell ID', 'Capacity [mAh]'], 
+                formats={ 'Cell ID': '%s', 'Capacity [mAh]': '%s'},
+                Writer=asciitable.FixedWidth)
+        else:
+            self.log.warning('no data')
+
+        print(','.join([ str(r[1]) for r in self.rows ]))
+
+        print("TOTAL Capacity [mAh]:")
+        print(sum([ float(r[1]) for r in self.rows ]))
+
+v1.register_report(v1.Report('capacity', CapacityReport))
---
+++ lib/python/secondlife/plugins/cell_identifiers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from secondlife.plugins.api import v1
+from structlog import get_logger
+
+class CellIdentifiers(object):
+
+    def __init__(self, **kwargs):
+        self.config = kwargs['config']
+        self.log = get_logger(name=__class__.__name__)
+        self.ids = []
+
+    def process_cell(self, path, metadata):
+        self.log = self.log.bind(path=path, metadata=metadata)
+        self.log.debug('processing cell')
+
+        self.ids.append(metadata.get('/id'))
+
+    def report(self):
+
+        print('\n'.join(self.ids))
+
+v1.register_report(v1.Report('cell_ids', CellIdentifiers))
