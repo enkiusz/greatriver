65e47a2763ced55b0cdde612d6b427de671d98f6
Maciej Grela <enki@fsck.pl>
2021-07-21 13:09:59 +0200

    secondlife: Simplify self-discharge reporting by providing just a PASS/FAIL flag

--- lib/python/secondlife/plugins/self_discharge.py
+++ lib/python/secondlife/plugins/self_discharge.py
@@ -17,25 +17,25 @@ class SelfDischargeReport(object):
         log = self.log.bind(id=infoset.fetch('.id'))
         log.debug('processing cell')
 
-        sd_rate = infoset.fetch('.state.self_discharge')
-        if sd_rate is not None:
-            self.data[infoset.fetch('.id')] = sd_rate
+        sd_check_res = infoset.fetch('.state.self_discharge.sd_check_result')
+        if sd_check_res is not None:
+            self.data[infoset.fetch('.id')] = sd_check_res
         else:
-            log.warn('no self-discharge estimation')
+            log.warn('no self-discharge check result')
 
     def report(self, format='ascii'):
         if format == 'ascii':
             if len(self.data.keys()) > 0:
-                asciitable.write([ (id, round(sd['v'], 3)) for (id, sd) in self.data.items() ],
-                    names=['Cell ID', 'Self-discharge [mV / day]'],
-                    formats={ 'Cell ID': '%s', 'Self-discharge [mV / day]': '%s'},
+                asciitable.write([ (id, sd_check_result) for (id, sd_check_result) in self.data.items() ],
+                    names=['Cell ID', 'Check result'],
+                    formats={ 'Cell ID': '%s', 'Check result': '%s'},
                     Writer=asciitable.FixedWidth)
             else:
                 self.log.warning('no data')
         else:
             log.error('unknown report format', format=format)
 
-class SelfDischargeRate(object):
+class SelfDischargeCheckResult(object):
     def __init__(self, **kwargs):
         self._cell = kwargs['cell']
 
@@ -61,7 +61,7 @@ class SelfDischargeRate(object):
             log.debug('measurements', entries=measurement_log)
 
             last_capacity_measurement_idx = next( 
-                filter( lambda idx: SelfDischargeRate._capacity_measurement(measurement_log[idx]), reversed(range(0, len(measurement_log))) ), 
+                filter( lambda idx: SelfDischargeCheckResult._capacity_measurement(measurement_log[idx]), reversed(range(0, len(measurement_log))) ),
                 None)
             if last_capacity_measurement_idx is None:
                 log.warn('no timestamped capacity measurement', id=self._cell.fetch('.id'))
@@ -71,7 +71,7 @@ class SelfDischargeRate(object):
             # Now search for the OCV measurement after capacity measurement
             # (assume the cell was charged up to 100% during the capacity measurement)
             ocv_measurement = next(
-                filter( lambda m: SelfDischargeRate._ocv_measurement(m), measurement_log[last_capacity_measurement_idx:] ),
+                filter( lambda m: SelfDischargeCheckResult._ocv_measurement(m), measurement_log[last_capacity_measurement_idx:] ),
                 None
             )
             if ocv_measurement is None:
@@ -86,17 +86,19 @@ class SelfDischargeRate(object):
                 log.warn('not enough days between capacity and OCV measurements', id=self._cell.fetch('.id'), T=T)
                 return None
             
-            # Assume the cell resting voltage is at 4.1V after charging
-            deltaV = 4.1 - ocv_measurement['results']['OCV']['v']
+            if ocv_measurement['results']['OCV']['v'] < 3.9:
+                result = 'FAIL'
+            else:
+                result = 'PASS'
 
-            log.debug('self-discharge estimation', id=self._cell.fetch('.id'), T=T, deltaV=deltaV)
+            log.debug('self-discharge check', id=self._cell.fetch('.id'), result=result)
 
-            return { 'v': (deltaV * 1000)/T, 'u': 'mV / day' }
+            return result
 
         except Exception as e:
             log.error('exception', _exc_info=e)
             return None
 
 
-v1.register_state_var('self_discharge', SelfDischargeRate)
+v1.register_state_var('self_discharge.sd_check_result', SelfDischargeCheckResult)
 v1.register_report(v1.Report('self_discharge', SelfDischargeReport))
