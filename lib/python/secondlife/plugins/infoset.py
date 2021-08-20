e9f6e6afd5f732ff8dc8471eca40f574fa05538c
Maciej Grela <enki@fsck.pl>
2021-08-20 11:16:52 +0200

    secondfar: Add infoset query feature to the infoset report

--- lib/python/secondlife/plugins/infoset.py
+++ lib/python/secondlife/plugins/infoset.py
@@ -5,6 +5,8 @@ from structlog import get_logger
 import asciitable
 import json
 
+from secondlife.cli.utils import CompileJQ
+
 class InfosetReport(object):
 
     def __init__(self, **kwargs):
@@ -16,7 +18,14 @@ class InfosetReport(object):
         log = self.log.bind(id=infoset.fetch('.id'))
         log.debug('processing cell')
 
-        self.cells[infoset.fetch('.id')] = infoset.to_json(indent=2)
+        json_text = infoset.to_json(indent=2)
+
+        if self.config.infoset_query:
+            # Apply the jq query if defined
+            json_text = self.config.infoset_query.input(text=json_text).text()
+            log.debug('jq query result', id=infoset.fetch('.id'), result=json_text, query=self.config.infoset_query)
+
+        self.cells[infoset.fetch('.id')] = json_text
 
     def report(self, format='ascii'):
         if format == 'ascii':
@@ -29,4 +38,9 @@ class InfosetReport(object):
         else:
             log.error('unknown report format', format=format)
 
+def _config_group(parser):
+    group = parser.add_argument_group('infoset report')
+    group.add_argument('--infoset-query', action=CompileJQ, help='Apply a JQ query to the infoset and print the result, use https://stedolan.github.io/jq/ syntax.')
+
 v1.register_report(v1.Report('infoset', InfosetReport))
+v1.register_config_group('infoset', _config_group)
