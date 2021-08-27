a19d97e990f9ffc840d6cdcefc5c028d813e5a6c
Maciej Grela <enki@fsck.pl>
2021-08-27 20:18:20 +0200

    secondlife: Support multiple queries in infoset report

--- lib/python/secondlife/plugins/infoset.py
+++ lib/python/secondlife/plugins/infoset.py
@@ -4,15 +4,22 @@ from secondlife.plugins.api import v1
 from structlog import get_logger
 import asciitable
 import json
+from collections import defaultdict
+import structlog
+import sys
+import jq
+import argparse
 
 from secondlife.cli.utils import CompileJQ
+
+log = structlog.get_logger()
 
 class InfosetReport(object):
 
     def __init__(self, **kwargs):
         self.config = kwargs['config']
         self.log = get_logger(name=__class__.__name__)
-        self.cells = dict()
+        self.cells = defaultdict(list)
 
     def process_cell(self, infoset):
         log = self.log.bind(id=infoset.fetch('.id'))
@@ -20,27 +27,46 @@ class InfosetReport(object):
 
         json_text = infoset.to_json(indent=2)
 
-        if self.config.infoset_query:
-            # Apply the jq query if defined
-            json_text = self.config.infoset_query.input(text=json_text).text()
-            log.debug('jq query result', id=infoset.fetch('.id'), result=json_text, query=self.config.infoset_query)
-
-        self.cells[infoset.fetch('.id')] = json_text
+        if len(self.config.infoset_queries) > 0:
+            # Apply the jq queries if defined
+            for query in self.config.infoset_queries:
+                result = query.input(text=json_text).text()
+                self.cells[infoset.fetch('.id')].append( result )
+                log.debug('jq query result', id=infoset.fetch('.id'), result=result, query=query)
+        else:
+            self.cells[infoset.fetch('.id')] = [ json_text ]
 
     def report(self, format='ascii'):
-        if format == 'ascii':
-            if len(self.cells.items()) > 0:
-                for (id, infoset) in self.cells.items():
-                    print(f"=== Infoset for {id}")
-                    print(infoset)
-            else:
-                self.log.warning('no data')
-        else:
+        if format != 'ascii':
             log.error('unknown report format', format=format)
+            return
+
+        if len(self.cells.items()) == 0:
+            self.log.warning('no data')
+            return
+
+        if len(self.config.infoset_queries) > 0:
+            print(self.cells)
+            asciitable.write([ [id] + [ result for result in self.cells[id] ] for id in self.cells.keys() ],
+                names=['Cell ID'] + [ query.program_string for query in self.config.infoset_queries ],
+                formats={ 'Cell ID': '%s' },
+                Writer=asciitable.FixedWidth)
+        else:
+            for (id, items) in self.cells.items():
+                print(f"=== Infoset for {id}")
+                print(infoset)
+
+class CompileJQAndAppend(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        try:
+            getattr(namespace, self.dest).append(jq.compile(values))
+        except Exception as e:
+            log.error('cannot compile query', query=values, _exc_info=e)
+            sys.exit(1)
 
 def _config_group(parser):
     group = parser.add_argument_group('infoset report')
-    group.add_argument('--infoset-query', action=CompileJQ, help='Apply a JQ query to the infoset and print the result, use https://stedolan.github.io/jq/ syntax.')
+    group.add_argument('--infoset-query', default=[], dest='infoset_queries', action=CompileJQAndAppend, help='Apply a JQ query to the infoset and print the result, use https://stedolan.github.io/jq/ syntax.')
 
 v1.register_report(v1.Report('infoset', InfosetReport))
 v1.register_config_group('infoset', _config_group)
