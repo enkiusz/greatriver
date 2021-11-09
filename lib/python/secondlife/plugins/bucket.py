d4d2681e0c1bb0cd29cb15075ccebb880508125c
Maciej Grela <enki@fsck.pl>
2021-11-09 20:05:57 +0100

    greatriver: Handle identifiers which do not follow the PREFIX~ID rule

--- lib/python/secondlife/plugins/bucket.py
+++ lib/python/secondlife/plugins/bucket.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 from secondlife.plugins.api import v1
+import structlog
+
+log = structlog.get_logger()
 
 
 def _odd_even(d):
@@ -12,14 +15,18 @@ class Bucket(object):
         self._cell = kwargs['cell']
 
     def fetch(self, path, default=None):
-        (prefix, num) = self._cell.fetch('.id').split('~')
+        try:
+            (prefix, num) = self._cell.fetch('.id').split('~')
 
-        if prefix is None or num is None:
-            return None
-        if len(num) < 3:
-            return None
+            if prefix is None or num is None:
+                return None
+            if len(num) < 3:
+                return None
 
-        return f'{num[0:2]}{_odd_even(int(num[2]))}'
+            return f'{num[0:2]}{_odd_even(int(num[2]))}'
+        except ValueError as e:
+            log.warn('cannot determine bucket', id=self._cell.fetch('.id'))
+            return None
 
 
 v1.register_state_var('bucket', Bucket)
